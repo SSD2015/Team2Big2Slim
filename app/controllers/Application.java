6c08ad6ce07d96d4f62e82809546f14ac4c45746
Nut Kaewnak <nut-boss@hotmail.com>
2015-03-04 15:50:07 +0700

    trying to link vote system
    Conflicts:
    	.gitignore

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,17 +2,26 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.Form;
+import play.db.ebean.Model;
+import models.dummyProject;
 
 import views.html.*;
 
 public class Application extends Controller {
+
+    public Application() {
+    }
 
     public static Result index() {
         return ok(index.render() );
     }
 
     public static Result voting() {
-        return ok(vote.render() );
+        dummyProject  project = Form.form(dummyProject.class).bindFromRequest().get();
+        project.save();
+        return redirect(routes.Application.index());
+        return ok(vote.render());
     }
 	
 	public static Result group1() {
--- app/models/dummyProject.java
+++ app/models/dummyProject.java
@@ -3,12 +3,13 @@ package models;
 import play.db.ebean.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class dummyProject extends Model  {
-        String ID;
-        String name;
-
-
+    @Id
+    String ID;
+    String name;
+    int score;
 
 }
