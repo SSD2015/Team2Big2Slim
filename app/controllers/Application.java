e9052696f48dc262aca417f933a475b24aebe44c
Nathakorn Sukumsirichart <korn.za@windowslive.com>
2015-03-05 13:28:06 +0700

    Add button, label and detail of example project
    Need to design better.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,14 +8,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-
         return ok(index.render("Your new application is ready."));
-
     }
     public static Result hello() {
-
         return ok("<h1>Hello World!</h1>");
-
     }
     public static Result profile()  {
         return ok(profile.render());
