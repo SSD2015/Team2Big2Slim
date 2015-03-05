badf0920b4bf934006fd48e96c077920dd81cd36
Nathakorn Sukumsirichart <korn.za@windowslive.com>
2015-03-05 14:34:46 +0700

    Add link in button to see full screenshot

--- target/scala-2.11/twirl/main/views/html/profile.template.scala
+++ target/scala-2.11/twirl/main/views/html/profile.template.scala
@@ -35,15 +35,23 @@ Seq[Any](format.raw/*1.1*/("""<!DOCTYPE HTML>
     <body>
         <h1>Profile: </h1>
         <p>
-            <img src = "http://static.sportskeeda.com/wp-content/uploads/2014/08/gta-v-1409058623.jpg"
-            alt = "Description" />
+            <img src = "http://static.sportskeeda.com/wp-content/uploads/2014/08/gta-v-1409058623.jpg" style="width:600px;height:350px"
         </p>
-        <h2>Description</h2>
+        <h2>Description:</h2>
         <p>Project name: GTA</p>
         <p>Type of project: Game</p>
         <p>Member: Korn , PP ,Mo , Sompat , Nut</p>
+        <h1>ScreenShot:</h1>
+        <p>
+            <button> <img src="http://i.ytimg.com/vi/TM6JCG6LIHo/maxresdefault.jpg" style="width:180px;height:128px" onclick="location.href='http://i.ytimg.com/vi/TM6JCG6LIHo/maxresdefault.jpg'"> </button>
+            <button> <img src="http://i.ytimg.com/vi/0YEY4PVbg0E/maxresdefault.jpg" style="width:180px;height:128px" onclick="location.href='http://i.ytimg.com/vi/0YEY4PVbg0E/maxresdefault.jpg'"> </button>
+            <button> <img src="http://i.ytimg.com/vi/lAdBJQXoCpc/maxresdefault.jpg" style="width:180px;height:128px" onclick="location.href='http://i.ytimg.com/vi/lAdBJQXoCpc/maxresdefault.jpg'"> </button>
+        </p>
     </body>
-    <button type="button" onclick="getElementById('content').innerHTML ='asdasdasd'">Read more!</button>
+    <button type="button" onclick="getElementById('content').innerHTML ='<p>Date start: 14/02/15<br> ' +
+    'Date finish : 20/3/15<br> </p>'">Read more!
+    </button>
+
     <div id="content"></div>
 </html>"""))}
   }
@@ -57,9 +65,9 @@ Seq[Any](format.raw/*1.1*/("""<!DOCTYPE HTML>
 }
               /*
                   -- GENERATED --
-                  DATE: Thu Mar 05 13:30:00 ICT 2015
+                  DATE: Thu Mar 05 14:31:45 ICT 2015
                   SOURCE: /Users/nathakorn/Documents/Team2Big2Slim/app/views/profile.scala.html
-                  HASH: 35392fe7ff92e78f7b2f146a857116cfef374e5a
+                  HASH: b3852397455a54afe3e50836c1064c69125e198c
                   MATRIX: 800->0
                   LINES: 29->1
                   -- GENERATED --
