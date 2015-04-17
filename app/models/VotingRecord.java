91e47311e20b15bea19423985672696de43e64d5
Momomomomo <topolite@hotmail.com>
2015-04-17 22:54:07 +0700

    create VotingRecord for record the vote

---
+++ app/models/VotingRecord.java
@@ -0,0 +1,40 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Id;
+
+public class VotingRecord {
+
+    @Id
+    public int ID;
+    public int userID;
+    public int criteriaID;
+    public int projectID;
+
+
+    public static Model.Finder<Integer, VotingRecord> find = new Model.Finder<Integer,VotingRecord>(
+            Integer.class, VotingRecord.class
+    );
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getProjectID() {
+        return this.projectID;
+    }
+
+    public int getCriteriaID() {
+        return this.criteriaID;
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
+
+    public void changeProject(int projectID) {
+        this.projectID = projectID;
+    }
+
+}
