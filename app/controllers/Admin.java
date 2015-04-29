d2f019cb1a0c508535d41010ad097e828c594580
Momomomomo <topolite@hotmail.com>
2015-04-30 00:52:18 +0700

    initialize admin page

---
+++ app/controllers/Admin.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import models.User;
+
+import play.*;
+import play.mvc.*;
+import views.html.adminMain;
+import views.html.index;
+
+public class Admin extends Controller {
+
+    @Security.Authenticated(Secured.class)
+    public static Result adminIndex() {
+        int currentUserID = Integer.parseInt(session().get("userID"));
+        User currentUser = User.find.byId( currentUserID );
+
+        if( currentUser.projectId == 99) {
+            return ok(adminMain.render());
+        }
+        else {
+            return redirect(routes.Application.index() );
+        }
+    }
+}
