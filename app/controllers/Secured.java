15e4d4fc1714b91ba53c19882b6a065a3f469e6b
Punpikorn <sadanthem@gmail.com>
2015-05-06 21:12:27 +0700

    add time's up method

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -3,9 +3,14 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import play.mvc.Http.*;
+import java.time.LocalDateTime;
 import models.*;
 
 public class Secured extends Security.Authenticator {
+    //system due time
+    public static int myHour = 20;
+    public static int myMinute = 46;
+    public static int mySecond = 0;
 
     @Override
     public String getUsername(Context ctx) {
@@ -15,5 +20,26 @@ public class Secured extends Security.Authenticator {
     @Override
     public Result onUnauthorized(Context ctx) {
         return redirect(routes.Login.login());
+    }
+
+    public static boolean TimesUp() {
+        int hour = LocalDateTime.now().getHour();
+        int minute = LocalDateTime.now().getMinute();
+        int second = LocalDateTime.now().getSecond();
+
+        if (hour > myHour) {
+            return true;
+        } else {
+            if (minute > myMinute) {
+                return true;
+            } else {
+                if (second > mySecond) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
     }
 }
