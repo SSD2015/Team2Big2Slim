3806ca649499801ceafab055575722c74fade025
Punpikorn <sadanthem@gmail.com>
2015-05-06 20:04:25 +0700

    add edit page controller

---
+++ app/controllers/EditPage.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import models.User;
+import play.mvc.Controller;
+import views.html.adminMain;
+import views.html.edit;
+import play.mvc.*;
+/**
+ * Created by Punpikorn on 5/6/15 AD.
+ */
+public class EditPage extends Controller{
+
+    public static Result typeCheck(Integer userType) {
+        if (userType == 88) {
+            return redirect("/");
+        }
+        else if (userType == 99) {
+            return ok(adminMain.render());
+        }
+        else {
+            return ok(edit.render(userType));
+        }
+    }
+}
